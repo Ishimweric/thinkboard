d57b4868c41c83b673eabe1d11d3343f3316e035
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-29 08:48:22 +0200

    add: Seted up initial routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
 import express from "express";
 const app = express();
 
+app.get("/api/notes" , (req, res)=>{
+  res.status(200).json({"message" : "notes sent successfully"});
+})
+
+app.post("/api/notes" , (req, res)=>{
+  res.status(201).json({"message" : "note created successfully"})
+})
+
+app.put("/api/notes/:id" , (req, res)=>{
+  res.status(200).json({"message" : "note updated succesfully"})
+})
+
+app.delete("/api/notes/:id" , (req, res)=>{
+  res.status(200).json({"message" : "note deleted successfully"})
+})
+
 app.listen(3500, ()=>{
   console.log("SERVER RUNNING ON PORT 3500")
 })
