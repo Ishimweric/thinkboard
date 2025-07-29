971b19c9ad374c3e9e51c72b6b5efecf0e1b349f
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-29 16:25:44 +0200

    feat: Added initial rate limiter

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,13 @@
+import {Ratelimit} from "@upstash/ratelimit"
+import {Redis} from "@upstash/redis"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+//limit to 5 req per 10 seconds
+const rateLimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(5,"10 s") // 5 req in 10 s
+})
+
+export default rateLimit
--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import Note from "../models/Notes.js"
 const getAllNotes = async (req, res)=>{
   try{
-    const notes = await Note.find({});
+    const notes = await Note.find({}).sort({"createdAt" : -1}); // newest first
     res.status(200).json(notes)
   }catch(err){
     res.status(500).json({"error" : "Internal server error"});
