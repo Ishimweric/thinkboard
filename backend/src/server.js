eb8c9b6cf6147d83bd3b5c7f009dab0067fd07a4
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-30 09:57:34 +0200

    add: Connected frontend to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,12 @@ import router from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors"
 
 dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 3500;
 
 connectDB().then(()=>{
   app.listen(PORT, ()=>{
@@ -13,9 +16,11 @@ connectDB().then(()=>{
   })
 })
 
-const PORT = process.env.PORT || 3500;
-
+//middlewares
 app.use(express.json());
-app.use(rateLimiter)
+app.use(rateLimiter);
+app.use(cors({
+  origin: "http://localhost:5173"
+}))
 
 app.use("/api/notes", router);
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
+import RateLimitedUI from "../components/RateLimitedUI"
+import axios from "axios"
 
 const HomePage = () => {
+  const [isRateLimited, setRateLimited]= useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const fetchNotes = async ()=>{
+      try {
+        const res = await axios.get("http://localhost:3500/api/notes");
+        console.log(res);
+        console.log(res.data)
+      }catch (err) {
+        console.error("Error fetching data", err)
+      }
+    }
+    fetchNotes();
+  },[])
+
   return (
     <div className='min-h-screen'>
       <Navbar/>
+      {isRateLimited && <RateLimitedUI/>}
     </div>
   )
 }
