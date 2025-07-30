4c8bbcb64527d679ef844022dc3df3926812ef64
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-30 09:10:20 +0200

    add: Added header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetails from './pages/NoteDetalisPage'
-import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>
+    <div data-theme = "forest">
       <Routes>
         <Route path='/' element = {<HomePage/>}/>
         <Route path='/create' element = {<CreatePage/>}/>
-        <Route path='/details' element = {<NoteDetails/>}/>
+        <Route path='/note/:id' element = {<NoteDetails/>}/>
       </Routes>
     </div>
   )
