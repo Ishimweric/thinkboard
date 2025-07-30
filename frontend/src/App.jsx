c66420b5b7b0e4d181f10e1cac56e1fcdf116866
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-30 07:28:59 +0200

    add: Created Rotes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetails from './pages/NoteDetalisPage'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element = {<HomePage/>}/>
+        <Route path='/create' element = {<CreatePage/>}/>
+        <Route path='/details' element = {<NoteDetails/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
