d895a92f51128b1a8915df3ea46654aa80100e1d
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-30 14:23:42 +0200

    add: Created empty notes component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from "../components/RateLimitedUI"
 import api from "../lib/axios"
 import toast from "react-hot-toast"
 import Notecard from "../components/Notecard"
+import EmptyNotes from "../components/EmptyNotes";
 
 const HomePage = () => {
   const [isRateLimited, setRateLimited]= useState(false);
@@ -36,6 +37,7 @@ const HomePage = () => {
       <Navbar/>
       {isRateLimited && <RateLimitedUI/>}
       <div className="max-w-7xl mx-auto p-4 mt-6">
+        {notes.length === 0 && !isRateLimited && !isLoading && <EmptyNotes/>}
         {isLoading && <div className="text-center text-primary py-10">Loading...</div>}
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
