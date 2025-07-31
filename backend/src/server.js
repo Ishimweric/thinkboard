0c514233817485b73f9e36edc98e6c2646e72e99
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-31 07:51:03 +0200

    add: Configured my files for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,12 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from "path"
 
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT || 3500;
+const __dirname = path.resolve()
 
 connectDB().then(()=>{
   app.listen(PORT, ()=>{
@@ -17,10 +18,23 @@ connectDB().then(()=>{
 })
 
 //middlewares
-app.use(cors({
-  origin: "http://localhost:5173"
-}))
+if (process.env.NODE_ENV !== "production"){
+  // only use cors in development
+  app.use(cors({
+    origin: "http://localhost:5173"
+  }))
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", router);
+
+//only do this in production
+if (process.env.NODE_ENV === "production"){
+  // for deployment
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
