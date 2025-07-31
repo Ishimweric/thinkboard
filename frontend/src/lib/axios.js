66b311862bb1d7046781422881591672380e9e69
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-31 08:27:28 +0200

    refactor(axios): Made baseurl dynamic for proper deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// changed to dynamic url for production purposes
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3500/api" : "/api"
+
 const api = axios.create({
-  baseURL : "http://localhost:3500/api"
+  baseURL : BASE_URL,
 });
 
 export default api
